1a414acf2dc667c217deb5c50f13bcd4081f195d
JandersonLSilva <janderson.user@gmail.com>
2024-11-14 20:40:28 -0300

    comcluindo a extração dos dados do log.txt

--- line_graphics.py
+++ line_graphics.py
@@ -1,25 +1,39 @@
 import re
 
 class Runtimes:
-    def __init__(self, sequential, parallel_tbb, parallel_omp):
-        self.sequ = sequential
-        self.tbb = parallel_tbb
-        self.omp = parallel_omp
-
+   def __init__(self, sequential, parallel_tbb, parallel_omp):
+       self.sequ = sequential
+       self.tbb = parallel_tbb
+       self.omp = parallel_omp
 
 def get_runtimes(file):
     with open(file, 'r') as file:
         text = file.read()
         runtimes = Runtimes(
-            re.findall(r"Sequencial - Tempo de execução: (\d+(\.\d+)?e-\d+)", text),
-            re.findall(r"Paralelo (openmp) - Tempo de execução: (\d+(\.\d+)?e-\d+)", text),
-            re.findall(r"Paralelo (tbb) - Tempo de execução: (\d+(\.\d+)?e-\d+)", text)
+            re.findall(r"Sequencial - Tempo de execução: (\d+(\.*\d*)e*-*\d*) segundos", text),
+            re.findall(r"Paralelo \(tbb\) - Tempo de execução: (\d+(\.*\d*)e*-*\d*) segundos", text),
+            re.findall(r"Paralelo \(openmp\) - Tempo de execução: (\d+(\.*\d*)e*-*\d*) segundos", text)
         )
     return runtimes
 
-for runtime in get_runtimes('./logs/log.txt').sequ:
-    print(f"Runtime Sequential: {runtime[0]}")
-    print('-'*80)
+runtimes = get_runtimes("./logs/log.txt")
+
+runtimes.sequ = list(map(lambda v: float(v[0]), runtimes.sequ))
+runtimes.tbb = list(map(lambda v: float(v[0]), runtimes.tbb))
+runtimes.omp = list(map(lambda v: float(v[0]), runtimes.omp))
+
+i = 0
+for runtime in runtimes.sequ:
+    i+=1
+    print(f"{i}° - Runtime Sequential: {runtime}")
+
+for runtime in runtimes.tbb:
+    i+=1
+    print(f"{i}° - Runtime Paralelo (tbb): {runtime}")
+
+for runtime in runtimes.omp:
+    i+=1
+    print(f"{i}° - Runtime Parelelo (openmp): {runtime}")
 
 # import matplotlib.pyplot as plt
 # import numpy as np
