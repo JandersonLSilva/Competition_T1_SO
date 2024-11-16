3c93e8983ee7282bd445988a753b7f5a7005e2f6
JandersonLSilva <janderson.user@gmail.com>
2024-11-15 22:26:07 -0300

    Organizando melhor o código, adcionando alguns comentários e criando a função que mostra os dados no formato de tabela

--- line_graphics.py
+++ line_graphics.py
@@ -1,5 +1,9 @@
 import re
+import pandas as pd # É preciso ter o pandas instalado (pip install pandas)
+import tabulate as tab # É necessário ter o tabulate instalado (pip install tabulate)
 
+
+# Classe de Execuções
 class Runtimes:
    def __init__(self, sequential, parallel_tbb, parallel_omp):
        # Mapeando os valores (vetores) do vetor recebido e substituindo sobre cada vetor a primeira posição do vetor, convertido para o tipo float, usando a função anônima lambda e depois atribuindo o vetor resultante ao próprio atributo da classe
@@ -8,6 +12,8 @@ class Runtimes:
        self.tbb = list(map(lambda v: float(v[0]), parallel_tbb))
        self.omp = list(map(lambda v: float(v[0]), parallel_omp))
 
+
+# Função que extrai os dados do arquivo
 def get_runtimes(file):
     with open(file, 'r') as file:
         text = file.read()
@@ -19,18 +25,18 @@ def get_runtimes(file):
     return runtimes
 
 
+# Função que mostra os dados no formato de tabela
+def show_table(data):
+    # Transformando o objeto em um DataFrame
+    data_frame = pd.DataFrame(data)
 
+    # Formatando a saida da tabela (DataFrame)
+    print(tab.tabulate(data_frame, headers='keys', tablefmt='grid', showindex=False))
 
 
 # Atribuindo o retorno da função get_runtimes a variável runtimes
 runtimes = get_runtimes("./logs/log.txt")
 LENGTH = len(runtimes.sequ)
-
-
-
-
-import pandas as pd # É preciso ter o pandas instalado (pip install pandas)
-import tabulate as tab # É necessário ter o tabulate instalado (pip install tabulate)
 
 # Criando um array com os números de execuções
 runs = list( map(lambda r: f"nº{str(r)}:", (range(1, LENGTH+1)) ) )
@@ -42,21 +48,8 @@ data = {
     'Parallel (tbb)': runtimes.tbb,
     'Parallel (openmp)': runtimes.omp
 }
-# Transformando o objeto em um DataFrame
-data_frame = pd.DataFrame(data)
+show_table(data)
 
-# Formatando a saida da tabela (DataFrame)
-print(tab.tabulate(data_frame, headers='keys', tablefmt='grid', showindex=False))
-
-
-
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-#
-# # Criando uma sequencia de números que será plotados no gráficos
-# runs = np.arange(1, 31)
-# runtimes_sequential = np.array()
 
 
 
